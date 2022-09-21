fb3009c7a27fdd9d97b8b48488e326bfc0241d9f
BT_4N2_03 <email@email>
2022-09-21 12:29:40 -0400

    I added some comments.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        //This is where the program will collect user input.
         System.out.print("Please input the total cost (without tip): ");
         double totalCost = s.nextDouble();
         System.out.print("Please input the percentage tipped (whole number without percent sign): ");
         int percentTipped = s.nextInt();
         System.out.print("Please input the amount of people that the cost will be split amongst: ");
         int numOfPeople = s.nextInt();
+            //This is where calculations are done for the amount of tip, amount of the tip paid per person, the bill including tip, and the amount of that bill paid per person.
             double totalTipped = Math.ceil(totalCost * percentTipped) / 100;
             double tipPerPerson = Math.ceil(totalTipped / numOfPeople * 100) / 100;
             double finalBill = Math.ceil((totalTipped + totalCost) * 100) / 100;
             double soloBillFinal = Math.ceil(finalBill / numOfPeople * 100) / 100;
-                System.out.println("");
+                //This is where the data is printed and outputted for the user to see.
+                System.out.println();
                 System.out.print("The total tip amount is: $");
                 System.out.format("%.2f%n", totalTipped);
                 System.out.print("The amount of tip paid per person is: $");
