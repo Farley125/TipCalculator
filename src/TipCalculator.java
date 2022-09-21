c08809541f1e4690c525785d98946db83b211c17
BT_4N2_03 <email@email>
2022-09-21 12:24:09 -0400

    I have added calculations to calculate the totalTipped, tipPerPerson, finalBill, and amount of the bill plus tip paid per person and added code which'll display the information to the user.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -8,5 +8,18 @@ public class TipCalculator {
         int percentTipped = s.nextInt();
         System.out.print("Please input the amount of people that the cost will be split amongst: ");
         int numOfPeople = s.nextInt();
+            double totalTipped = Math.ceil(totalCost * percentTipped) / 100;
+            double tipPerPerson = Math.ceil(totalTipped / numOfPeople * 100) / 100;
+            double finalBill = Math.ceil((totalTipped + totalCost) * 100) / 100;
+            double soloBillFinal = Math.ceil(finalBill / numOfPeople * 100) / 100;
+                System.out.println("");
+                System.out.print("The total tip amount is: $");
+                System.out.format("%.2f%n", totalTipped);
+                System.out.print("The amount of tip paid per person is: $");
+                System.out.format("%.2f%n", tipPerPerson);
+                System.out.print("The total bill (including tip) is: $");
+                System.out.format("%.2f%n", finalBill);
+                System.out.print("Bill evenly split among patrons is: $");
+                System.out.format("%.2f%n", soloBillFinal);
     }
 }
